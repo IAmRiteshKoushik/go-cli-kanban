0405fd7b19202c8f9cfd6a44e1d89079006a800d
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-01-02 22:03:58 +0530

    add: charmbracelet/bubbles/list package

--- main.go
+++ main.go
@@ -1,3 +1,44 @@
 package main
 
+import(
+  "github.com/charmbracelet/bubbles/list"
+)
 
+type status int
+
+// Indices to determine the lists
+const (
+	todo status = iota
+	inProgress
+	done
+)
+
+/* CUSTOM ITEM */
+type Task struct {
+	status      status
+	title       string
+	description string
+}
+
+// implement the list.Item interface
+func (t Task) FilterValue() string { // Filter based on title
+  return t.title
+}
+
+func (t Task) Title() string {
+  return t.title
+}
+
+func (t Task) Description() string {
+  return t.description
+}
+
+/* MAIN MODEL */
+type Model struct {
+  list list.Model
+  err error
+}
+
+func (m *Model) initList() {
+  m.list = list.New([]list.Item{}, list.NewDefaultDelegate())
+}
